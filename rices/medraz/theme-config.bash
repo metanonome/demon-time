9d97d3e0776a49dd618057f2a08de24ff7c7fbd6
metanonome <ruan.victor707@gmail.com>
2026-06-18 04:23:01 -0300

    feat: initial upload of demon-time files and medraz rice customization

---
+++ rices/medraz/theme-config.bash
@@ -0,0 +1,110 @@
+#############################
+#		Demon Time		#
+#############################
+# Copyright (C) 2021-2026 metanonome
+# https://github.com/metanonome/dotfiles
+
+# (Tokyo Night) colorscheme
+bg="#0d0d0d"
+fg="#f0f0f0"
+
+black="#111111"
+red="#a80000"
+green="#e53935"
+yellow="#c95545"
+blue="#448aff"
+magenta="#9a2a2a"
+cyan="#40c4ff"
+white="#d4d4d4"
+blackb="#555555"
+redb="#d44030"
+greenb="#ff5252"
+yellowb="#f05a48"
+blueb="#82b1ff"
+magentab="#b03030"
+cyanb="#80d8ff"
+whiteb="#f0f0f0"
+
+accent_color="#8b0000"
+arch_icon="#8b0000"
+
+# Bspwm options
+BORDER_WIDTH="1"		# Bspwm border
+TOP_PADDING="22"
+BOTTOM_PADDING="1"
+LEFT_PADDING="1"
+RIGHT_PADDING="1"
+NORMAL_BC="#BEC6F0"		# Normal border color
+FOCUSED_BC="#990202"	# Focused border color
+
+# Terminal font & size
+term_font_size="10"
+term_font_name="JetBrainsMono Nerd Font"
+
+# Picom options
+P_FADE="true"			# Fade true|false
+P_SHADOWS="true"		# Shadows true|false
+SHADOW_C="#1D0C0C"		# Shadow color
+P_CORNER_R="7"			# Corner radius (0 = disabled)
+P_BLUR="false"			# Blur true|false
+P_ANIMATIONS="@"		# (@ = enable) (# = disable)
+P_TERM_OPACITY="0.9"	# Terminal transparency. Range: 0.1 - 1.0 (1.0 = disabled)
+
+# Dunst
+dunst_offset='(20, 60)'
+dunst_origin='top-right'
+dunst_transparency='0'
+dunst_corner_radius='6'
+dunst_font='JetBrainsMono NF Medium 9'
+dunst_border='0'
+dunst_frame_color="$accent_color"
+dunst_icon_theme="TokyoNight-SE"
+# Dunst animations
+dunst_close_preset="fly-out"
+dunst_close_direction="up"
+dunst_open_preset="fly-in"
+dunst_open_direction="up"
+
+# Jgmenu colors
+jg_bg="$bg"
+jg_fg="$fg"
+jg_sel_bg="$accent_color"
+jg_sel_fg="$fg"
+jg_sep="$blackb"
+
+# Rofi menu font and colors
+rofi_font="JetBrainsMono NF Bold 9"
+rofi_background="$bg"
+rofi_bg_alt="$accent_color"
+rofi_background_alt="${bg}E0"
+rofi_fg="$fg"
+rofi_selected="$blue"
+rofi_active="$green"
+rofi_urgent="$red"
+
+# Screenlocker
+sl_bg="${bg}"
+sl_fg="${fg}"
+sl_ring="${black}"
+sl_wrong="${red}"
+sl_date="${fg}"
+sl_verify="${green}"
+
+# Gtk theme
+gtk_theme="z0mbi3Night-zk"
+gtk_icons="TokyoNight-SE"
+gtk_cursor="Qogirr-Dark"
+geany_theme="z0mbi3-TokyoNight"
+
+# Wallpaper engine
+# Available engines:
+# - Random  (Set a random wallpaper from Walls rice directory)
+# - CustomDir   (Set a random wallpaper from the directory you specified)
+# - Default (Sets a specific image as wallpaper) *Default
+# - Animated (Set an animated wallpaper. "mp4, mkv, gif")
+# - Slideshow (Change randomly every 15 minutes your wallpaper from Walls rice directory)
+ENGINE="Default"
+
+CUSTOM_DIR="/path/to/your/wallpapers/directory"
+DEFAULT_WALL="/home/medraz/.config/bspwm/rices/medraz/walls/yoi.jpeg"
+ANIMATED_WALL="$HOME/.config/bspwm/config/assets/animated_wall.mp4"
